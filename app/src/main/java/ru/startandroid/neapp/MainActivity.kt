000d2f06908eb9430b2d5264e0ce38ebb1d24de2
dmitry <zhdanovheavy@yandex.ru>
2022-10-03 17:40:11 +0300

    lets start it ver.2.0.1

--- app/src/main/java/ru/startandroid/neapp/MainActivity.kt
+++ app/src/main/java/ru/startandroid/neapp/MainActivity.kt
@@ -2,10 +2,33 @@ package ru.startandroid.neapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.startandroid.neapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val _mBinding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.fragment_splash)
+        Handler (Looper.myLooper()!!).postDelayed({
+            setContentView(_mBinding.root)
+            bottom_nav_menu.setupWithNavController(
+                navController = nav_host_fragment.findNavController()
+            )
+        },5000)
+    }
+
+    override fun onDestroy() {
+         super.onDestroy()
+        _binding = null
     }
 }
