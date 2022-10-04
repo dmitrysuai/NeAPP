7c7343b54a6493fd5f60bb0496bf77fc9b6d5664
dmitry <zhdanovheavy@yandex.ru>
2022-10-04 17:06:37 +0300

    lets start it ver.2.0.2 (ZAGRUZOCHKA)

--- app/src/main/java/ru/startandroid/neapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/ru/startandroid/neapp/ui/splash/SplashFragment.kt
@@ -1,12 +1,11 @@
 package ru.startandroid.neapp.ui.splash
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.startandroid.neapp.R
-import ru.startandroid.neapp.databinding.FragmentDetailBinding
+import androidx.fragment.app.Fragment
 import ru.startandroid.neapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -14,11 +13,14 @@ class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
     private val mBinding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         _binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
         return mBinding.root
     }
 }
+
