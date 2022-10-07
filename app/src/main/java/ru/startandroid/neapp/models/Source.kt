459306b33090df34e7ef6cf68a1683ede5e482ef
dmitry <zhdanovheavy@yandex.ru>
2022-10-07 14:25:51 +0300

    lets start it ver.3

---
+++ app/src/main/java/ru/startandroid/neapp/models/Source.kt
@@ -0,0 +1,6 @@
+package ru.startandroid.neapp.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/ru/startandroid/neapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package ru.startandroid.neapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL ="https://newsapi.org/"
+        const val API_KEY ="84ab22ef7275416a9e395ec58a9176a5"
+    }
+}
